cdfa5846efa70933b530d6dba9dab40013e12abc
KanaadPathak <kanaadpathak@gmail.com>
2018-02-11 21:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,13 @@ def makeWebhookResult(req):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+ON_HEROKU = os.environ.get('ON_HEROKU')
+if ON_HEROKU:
+    # get the heroku port 
+    port = int(os.environ.get("PORT", 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
+ #   port = int(os.getenv('PORT', 5000))
 
     print "Starting app on port %d" % port
 
